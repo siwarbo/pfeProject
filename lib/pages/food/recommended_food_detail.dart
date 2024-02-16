daa10c67872435f45053b268b38c0c271ad12fe9
Siwar Ben Othmen <siwarbenothmane@gmail.com>
2024-02-16 21:26:30 +0100

    Home Page Builded

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -11,64 +11,62 @@ class RecommendedFoodDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            toolbarHeight: 70,
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: Colors.white,
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              toolbarHeight: 70,
+              title: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  AppIcon(icon: Icons.clear),
+                  AppIcon(icon: Icons.shopping_cart_outlined)
+                ],
+              ),
+              bottom: PreferredSize(
+                preferredSize: Size.fromHeight(20),
+                child: Container(
+                  child: Center(
+                      child: BigText(
+                    size: Dimensions.font26,
+                    text: "Tunisian Plat",
+                  )),
+                  width: double.maxFinite,
+                  padding: EdgeInsets.only(top: 5, bottom: 10),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(Dimensions.radius20),
+                        topRight: Radius.circular(Dimensions.radius20),
+                      )),
+                ),
+              ),
+              pinned: true,
+              backgroundColor: AppColors.yellowColor,
+              expandedHeight: 300,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.asset(
+                  "assets/tunisian-dishes-mechouia-salad.jpg",
+                  width: double.maxFinite,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(
+                child: Column(
               children: [
-                AppIcon(icon: Icons.clear),
-                AppIcon(icon: Icons.shopping_cart_outlined)
+                Container(
+                  child: ExpandableTextWidget(
+                      text:
+                          "Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food "),
+                  margin: EdgeInsets.only(
+                      left: Dimensions.width20, right: Dimensions.width20),
+                )
               ],
-            ),
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(20),
-              child: Container(
-                child: Center(
-                    child: BigText(
-                  size: Dimensions.font26,
-                  text: "Tunisian Plat",
-                )),
-                width: double.maxFinite,
-                padding: EdgeInsets.only(top: 5, bottom: 10),
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(Dimensions.radius20),
-                      topRight: Radius.circular(Dimensions.radius20),
-                    )),
-              ),
-            ),
-            pinned: true,
-            backgroundColor: AppColors.yellowColor,
-            expandedHeight: 300,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Image.asset(
-                "assets/tunisian-dishes-mechouia-salad.jpg",
-                width: double.maxFinite,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          SliverToBoxAdapter(
-              child: Column(
-            children: [
-              Container(
-                child: ExpandableTextWidget(
-                    text:
-                        "Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food Tunisian food "),
-                margin: EdgeInsets.only(
-                    left: Dimensions.width20, right: Dimensions.width20),
-              )
-            ],
-          ))
-        ],
-      ),
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
+            ))
+          ],
+        ),
+        bottomNavigationBar: Column(mainAxisSize: MainAxisSize.min, children: [
           Container(
             padding: EdgeInsets.only(
               left: Dimensions.width20 * 2.5,
@@ -96,9 +94,55 @@ class RecommendedFoodDetail extends StatelessWidget {
                     icon: Icons.add)
               ],
             ),
-          )
-        ],
-      ),
-    );
+          ),
+          Container(
+            height: Dimensions.bottomHeightBar,
+            padding: EdgeInsets.only(
+                top: Dimensions.height30,
+                bottom: Dimensions.height30,
+                left: Dimensions.width30,
+                right: Dimensions.width30),
+            decoration: BoxDecoration(
+                color: AppColors.buttonBackgroundColor,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(Dimensions.radius20 * 2),
+                    topRight: Radius.circular(Dimensions.radius20 * 2))),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimensions.radius20),
+                    color: Colors.white,
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.mainColor,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                      top: Dimensions.height20,
+                      bottom: Dimensions.height20,
+                      left: Dimensions.width20,
+                      right: Dimensions.width20),
+                  child: BigText(
+                    text: "\£10 | Add to cart",
+                    color: Colors.white,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(Dimensions.radius20),
+                    color: AppColors.mainColor,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]));
   }
 }
